9be6bc6096f9990f8293bc633dedcb6f371005e1
VIKAS YADAV <79438544+vikas21222@users.noreply.github.com>
2026-03-05 15:40:21 +0000

    feat: add image upload endpoint and update project configuration

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends,File, UploadFile
 from sqlalchemy.orm import Session
 from app.database import engine, Base, get_db
 from app.models.photo import Photo
+import os
+import shutil
+from datetime import datetime
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 # Create tables
 Base.metadata.create_all(bind=engine)
+
+
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 @app.get("/")
 async def root():
@@ -39,6 +46,32 @@ async def get_photos(db: Session = Depends(get_db)):
         for photo in photos
     ]
 
+@app.post("/upload-photo")
+async def upload_photo(
+    file: UploadFile = File(...),
+    db: Session = Depends(get_db)
+):
+    # uploads folder create if not exists
+    os.makedirs("uploads", exist_ok=True)
+
+    # unique filename
+    timestamp = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    file_location = f"uploads/{timestamp}_{file.filename}"
+
+    # save file
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    # save in DB
+    new_photo = Photo(image_url=file_location)
+    db.add(new_photo)
+    db.commit()
+    db.refresh(new_photo)
+
+    return {
+        "id": new_photo.id,
+        "image_url": new_photo.image_url
+    }
 
 # python -m venv venv
 # source venv/bin/activate
